145fc8f9b9cdcb7ac0914074ff65fe060a28aca9
Nicole Law <nicole.law12@gmail.com>
2023-08-02 13:57:22 -0400

    counts done

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["JUNGLE_USERNAME"], password: ENV["JUNGLE_PASSWORD"]
   def show
+    @products = Product.all 
+    @categories = Category.all
+    @products_count = @products.count 
+    @categories_count = @categories.count 
   end
 end
