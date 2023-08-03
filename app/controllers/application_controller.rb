db5ccfe7b796486b64c11a6f356c7f527db2d0a5
Nicole Law <nicole.law12@gmail.com>
2023-08-03 18:18:57 -0400

    login/logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,16 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  def current_user 
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # @current_user = session[:user_id] ? User.find(session[:user_id]) : nil
+  end 
+  helper_method :current_user 
+
+  def authorize 
+    redirect_to '/login' unless current_user
+  end 
+
   private
 
   def cart
