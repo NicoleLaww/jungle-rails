43955c11fc4b86040da3d82638a2f708990964b1
Nicole Law <nicole.law12@gmail.com>
2023-08-17 17:01:46 -0400

    cypress products page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,8 @@
+describe('Product Page', () => {
+  it('should redirect to product detail page when clicking on 1st product/Scented Blade', () => {
+    cy.visit('/');
+    cy.get('.products article').first().find('button.btn').click();
+    cy.visit('/products/2');
+    cy.get('.product-detail div h1').contains('Scented Blade');
+  });
+})
