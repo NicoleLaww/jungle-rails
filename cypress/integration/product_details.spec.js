82dee73a7442722d9b8f87817451581ee41d39e8
Nicole Law <nicole.law12@gmail.com>
2023-08-17 17:22:54 -0400

    fixed minor css

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,7 +1,7 @@
 describe('Product Page', () => {
   it('should redirect to product detail page when clicking on 1st product/Scented Blade', () => {
     cy.visit('/');
-    cy.get('.products article').first().find('button.btn').click();
+    cy.get('.products article').first().find('button.btn').click({force: true});
     cy.visit('/products/2');
     cy.get('.product-detail div h1').contains('Scented Blade');
   });
